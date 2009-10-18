c76dd9b5b2ea93e254d58e512bcee4b631dd4c5c
Benjamin Bentmann <bentmann@apache.org>
2009-10-18 13:43:36 +0000

    o Fixed ITs
    git-svn-id: https://svn.apache.org/repos/asf/maven/resources/trunk/apache-source-release-assembly-descriptor@826438 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/its/IT_005_MiscellaneousExcludes.java
+++ src/test/java/org/apache/its/IT_005_MiscellaneousExcludes.java
@@ -19,7 +19,7 @@ import java.util.Set;
 public class IT_005_MiscellaneousExcludes
 {
     
-    private static final String BASENAME = "shared-resources";
+    private static final String BASENAME = "misc-excludes";
     private static final String VERSION = "1";
     
     @Test
