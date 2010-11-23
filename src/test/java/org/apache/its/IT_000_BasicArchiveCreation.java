dd5c514fae58a0683f190d142dba451eca46021c
Brian Demers <bdemers@apache.org>
2010-11-23 14:35:06 +0000

    Adding a zip and tar source assembly, some apache projects need zips and tars for source distributions
    git-svn-id: https://svn.apache.org/repos/asf/maven/resources/trunk/apache-source-release-assembly-descriptor@1038139 13f79535-47bb-0310-9956-ffa450edef68

--- src/test/java/org/apache/its/IT_000_BasicArchiveCreation.java
+++ src/test/java/org/apache/its/IT_000_BasicArchiveCreation.java
@@ -35,6 +35,8 @@ import java.util.Collections;
 import java.util.HashSet;
 import java.util.Set;
 
+import junit.framework.Assert;
+
 public class IT_000_BasicArchiveCreation
 {
     
@@ -53,6 +55,10 @@ public class IT_000_BasicArchiveCreation
         
         verifier.verifyErrorFreeLog();
         verifier.resetStreams();
+        
+       // make sure the tar did NOT get created by default
+        File tarAssemblyFile = new File( testDir, "target/" + BASENAME + "-" + VERSION + "-source-release.tar.gz" );
+        Assert.assertFalse( "tar assembly should not have been created", tarAssemblyFile.exists() );
         
         File assembly = new File( testDir, "target/" + BASENAME + "-" + VERSION + "-source-release.zip" );
         
