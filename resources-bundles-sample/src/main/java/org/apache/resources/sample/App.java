f952b9374b777c11f5fde965af2e069c60f02f6c
Olivier Lamy <olamy@apache.org>
2012-07-06 12:27:39 +0000

    add sample of using resources bundle
    git-svn-id: https://svn.apache.org/repos/asf/maven/resources/trunk@1358156 13f79535-47bb-0310-9956-ffa450edef68

---
+++ resources-bundles-sample/src/main/java/org/apache/resources/sample/App.java
@@ -0,0 +1,13 @@
+package org.apache.resources.sample;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
